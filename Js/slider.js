6f5839db9ebbb7c48b3a15a653fc4457023c2f2e
Hussein <husainanabtawi2001@hotmail.com>
2022-03-05 18:40:38 +0300

    Almost Done FirtstPage

---
+++ Js/slider.js
@@ -0,0 +1,50 @@
+$(document).ready(function(){
+    $('.slider').slick({
+    centerMode: true,
+    centerPadding: '60px',
+    slidesToShow:3,
+    rtl:true,
+    responsive: [
+        {
+        breakpoint: 768,
+        settings: {
+            arrows: false,
+            centerMode: true, 
+            slidesToShow:3
+        }
+        },
+        {
+        breakpoint: 480,
+        settings: {
+            arrows: false,
+            centerMode: true,
+            slidesToShow: 1
+        }
+        }
+    ]
+    });
+    $('.main-slider').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        fade: false,
+        useTransform: true,
+        infinite:true,
+        rtl:true,
+
+      });
+      $('.nav-slider').slick({
+        slidesToShow: 4,
+        slidesToScroll:1,
+        arrows:false,
+        centerMode:false,
+        
+        asNavFor: '.main-slider',
+        focusOnSelect: true,
+        verticalSwiping:true,
+        vertical:true,
+
+      });
+      
+
+});
