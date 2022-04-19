77e7eb8c870cac199682c9d2964636643828806c
Hussein <husainanabtawi2001@hotmail.com>
2022-04-20 01:31:52 +0300

    Some Updates

--- Js/base.js
+++ Js/base.js
@@ -2,3 +2,11 @@ window.addEventListener("load",function() {
     let loading = document.getElementById('Loading');
     loading.style.display='none';
 });
+function toggleAudio() {
+    let music=document.querySelector('audio');
+    music.muted = !music.muted ;
+    let icon = document.querySelector('#Volume-Icon');
+    icon.classList.toggle('fa-volume-up');
+    icon.classList.toggle('fa-volume-mute');
+
+}
